35960e702c01ebd5c2e3a0d8a5a35466406e7b91
Arunn <arunn.lingeswaran@hva.nl>
2023-04-19 08:31:27 +0200

    Updated printing json in weather_API.py

--- 03 weather_prediction/weather_API.py
+++ 03 weather_prediction/weather_API.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pprint
 
 with open('../config.json') as f:
     config = json.load(f)
@@ -22,5 +23,5 @@ def get_current_weather_data(lat_long):
     return data
 
 
-# use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
-print(json.dumps(get_current_weather_data("52.374_4.890"), indent=4, separators=(". ", " = ")))
+print(pprint.pprint(get_current_weather_data("52.374_4.890")))
+
