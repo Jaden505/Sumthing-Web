10353f5842eb58a709e315ba4e51ce4369a14375
Arunn <arunn.lingeswaran@hva.nl>
2023-04-26 13:29:20 +0200

    Created multiple weather functions

--- 03 weather_prediction/weather_API.py
+++ 03 weather_prediction/weather_API.py
@@ -15,8 +15,32 @@ def get_weather_data(lat_long, date):
 
 
 # Retrieve current weather data by latitude and longitude
-def get_current_weather_data(lat_long):
-    url = f'http://api.weatherstack.com/current?access_key={config["weather-api-key"]}&query={lat_long}'
+# @query: Search by latitude and longitude, example: 52.374_4.890
+# Subscription: All plans
+def get_current_weather_data(query):
+    url = f'http://api.weatherstack.com/current?access_key={config["weather-api-key"]}&query={query}'
+    res = requests.get(url)
+    data = res.json()
+
+    return data
+
+
+# Return weather forecast up to 14 days in the future
+# @query: Search by city name or country name
+# Subscription: Professional plan and higher
+def predict_weather(query):
+    url = f'http://api.weatherstack.com/forecast?access_key={config["weather-api-key"]}&query={query}&forecast_days=1&hourly=1'
+    res = requests.get(url)
+    data = res.json()
+
+    return data
+
+
+# Return the location by location search
+# @query: Search by city name or country name
+# Subscription: Standard plan and higher
+def location_lookup(query):
+    url = f'http://api.weatherstack.com/autocomplete?access_key={config["weather-api-key"]}&query={query}'
     res = requests.get(url)
     data = res.json()
 
@@ -24,4 +48,4 @@ def get_current_weather_data(lat_long):
 
 
 print(pprint.pprint(get_current_weather_data("52.374_4.890")))
-
+# print(pprint.pprint(location_lookup("Amsterdam")))
