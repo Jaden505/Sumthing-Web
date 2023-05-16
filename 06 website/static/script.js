d189a7add3dd108fc943a0860adbd788021788d1
Arunn <arunn.lingeswaran@hva.nl>
2023-05-16 14:28:21 +0200

    Replaced weather forecast table with graph

--- 06 website/static/script.js
+++ 06 website/static/script.js
@@ -8,19 +8,19 @@ async function updateSelectedImage(img) {
 
     const imageData = await getImageData(img_name);
 
-    const weatherForecastTable = document.querySelector(".weather-forecast-table");
+    const weatherForecastChart = document.querySelector("#weatherChart");
     const weatherInfoSection = document.querySelector(".weather-info");
 
     if (imageData[0] === undefined) {
-        weatherForecastTable.style.display = "none"
+        weatherForecastChart.style.display = "none"
         // Clear table content, if previous image had data
-        weatherForecastTable.innerHTML = ""
+
     } else {
-        weatherForecastTable.style.display = "block"
+        // weatherForecastTable.style.display = "block"
         weatherInfoSection.style.display = "block"
 
         const forecastData = await getWeatherForecast(imageData[0].latitude, imageData[0].longitude)
-        buildWeatherForecastTable(forecastData);
+        buildWeatherGraph(forecastData);
     }
 }
 
@@ -53,6 +53,7 @@ async function getWeatherForecast(latitude, longitude) {
 
     date.setDate(date.getDate() + 2);
     const futureDate = date.toISOString().slice(0, 10);
+
     const responseData = await response.json();
 
     // Extract the hourly temperature data
@@ -61,15 +62,17 @@ async function getWeatherForecast(latitude, longitude) {
     // Extract the hourly time data
     const hourlyTime = responseData.hourly.time;
 
-    // Get the indices for the desired date range (from May 15th to May 15th)
+    // Get the indices for the desired date range
     const startDate = new Date(currentDate + 'T00:00');
     const endDate = new Date(currentDate + 'T23:00');
 
     const startIndex = hourlyTime.findIndex(time => new Date(time) >= startDate);
+
+    // Subtracting 1 so the last element before falling out of range is included
     const endIndex = hourlyTime.findIndex(time => new Date(time) > endDate) - 1;
 
-
     // Slice the arrays to get the data for the desired date range
+    // Summing 1 so the last element is included
     const hourlyTemperatureRange = hourlyTemperature.slice(startIndex, endIndex + 1);
     const hourlyTimeRange = hourlyTime.slice(startIndex, endIndex + 1);
 
@@ -84,46 +87,46 @@ async function getWeatherForecast(latitude, longitude) {
     }));
 }
 
-/**
- * Creates the weather forecast table
- * @param data
- */
-function buildWeatherForecastTable(data) {
-    // Create the table element
-    let table = document.createElement('table');
+let chartInstance = null;
 
-    // Create the table header
-    let thead = document.createElement('thead');
-    let headerRow = document.createElement('tr');
-    let timeHeaderCell = document.createElement('th');
-    let temperatureHeaderCell = document.createElement('th');
-    timeHeaderCell.textContent = 'Time';
-    temperatureHeaderCell.textContent = 'Temperature';
-    headerRow.appendChild(timeHeaderCell);
-    headerRow.appendChild(temperatureHeaderCell);
-    thead.appendChild(headerRow);
-    table.appendChild(thead);
-
-    // Create the table body
-    let tbody = document.createElement('tbody');
-    for (let i = 0; i < data.length; i++) {
-        let row = document.createElement('tr');
-        let timeCell = document.createElement('td');
-        let temperatureCell = document.createElement('td');
-        timeCell.textContent = data[i].readable_time;
-        temperatureCell.textContent = data[i].temperature + ' °C';
-        row.appendChild(timeCell);
-        row.appendChild(temperatureCell);
-        tbody.appendChild(row);
+function buildWeatherGraph(data) {
+    // Destroy the existing chart if it exists
+    if (chartInstance) {
+        chartInstance.destroy();
     }
-    table.appendChild(tbody);
+    // Extract the time and temperature data from the weather data
+    const labels = data.map(entry => entry.readable_time);
+    const temperatures = data.map(entry => entry.temperature);
 
-    // Clear the weatherData div
-    let weatherDataDiv = document.querySelector('.weather-forecast-table');
-    weatherDataDiv.innerHTML = '';
+    // Create a new chart instance
+    const ctx = document.querySelector('#weatherChart').getContext('2d');
 
-    // Append the table to the weatherData div
-    weatherDataDiv.appendChild(table);
+    chartInstance = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Temperature',
+                    data: temperatures,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)', // Set the background color of the chart area
+                    borderColor: 'rgba(75, 192, 192, 1)', // Set the line color
+                    borderWidth: 1 // Set the line width in pixels
+                }
+            ]
+        },
+        options: {
+            responsive: true, // Make the chart responsive
+            scales: {
+                y: {
+                    beginAtZero: true // Start the y-axis from zero
+                },
+                x: {
+                    position: 'top'
+                }
+            },
+        }
+    });
 }
 
 
