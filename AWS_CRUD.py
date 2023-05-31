76609987cd6746695439bbe25b6645eacfcc2959
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-05-31 10:34:32 +0200

    fixed conflict
    Former-commit-id: 1fa33b2054a261b85a38ea88044f556a78290375

--- AWS_CRUD.py
+++ AWS_CRUD.py
@@ -1,5 +1,7 @@
 import boto3, json, os
 from PIL import Image
+import numpy as np
+import cv2
 
 def conn_AWS():
     # Read AWS credentials and bucket configuration from JSON file
@@ -27,6 +29,48 @@ def update_img(s3, bucket_name, folder_name, file_path):
 
 def del_img(s3, bucket_name, folder_name, file_name):
     s3.delete_object(Bucket=bucket_name, Key=folder_name + '/' + file_name)
+
+def read_images_from_s3(s3, bucket_name, folder_name):
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_name + '/')
+    objects = response.get('Contents', [])
+
+    train_images = []
+    train_labels = []
+
+    for obj in objects:
+        image_key = obj['Key']
+        image_bytes = s3.get_object(Bucket=bucket_name, Key=image_key)['Body'].read()
+        np_array = np.frombuffer(image_bytes, np.uint8)
+        img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        train_images.append(img)
+
+        label = image_key.split('/')[1].split('_')[0]
+        train_labels.append(label)
+
+    return train_images, train_labels
+
+def DownloadWeatherImages(local_folder):
+    train_images = []
+    train_labels = []
+    shape = (300, 300)
+    AWS_folder = 'weather/updated_dataset'
+
+    s3, bucket_name = conn_AWS()
+    os.mkdir(local_folder)
+
+    for image_path in get_images(s3, bucket_name, AWS_folder):
+        if image_path.lower().endswith('.jpg') or image_path.lower().endswith(
+                '.jpeg') or image_path.lower().endswith('.png'):
+            local_filepath = os.path.join(local_folder, os.path.basename(image_path))
+            s3.download_file(bucket_name, image_path, local_filepath)
+
+            img = cv2.imread(local_filepath)
+            train_labels.append(os.path.basename(image_path).split('_')[0])
+            img = cv2.resize(img / 255, shape)
+            train_images.append(img)
+
+    return train_images, train_labels
+
 
 def upload_weather_images(s3, bucket_name, folder_name, local_folder_path):
     image_name_changer(local_folder_path, '.jpg', local_folder_path)
