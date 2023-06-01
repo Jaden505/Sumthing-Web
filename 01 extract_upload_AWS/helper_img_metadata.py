09c271c4a35f265ba99f28266dee47d38aff8160
Ayoub Ez-Zaouia <ayoub.ez-zaouia@hva.nl>
2023-06-01 16:44:23 +0200

    Removed load images from database function and created array function
    Former-commit-id: 40c41d68e6a28d314594d2d937479b473c660be0

--- 01 extract_upload_AWS/helper_img_metadata.py
+++ 01 extract_upload_AWS/helper_img_metadata.py
@@ -2,20 +2,23 @@
 
 from PIL.ExifTags import TAGS, GPSTAGS
 
+
 # Conversion latitude longitude
 def decimal_coords(coords, ref):
- decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
- if ref == 'S' or ref == 'W':
-     decimal_degrees = -decimal_degrees
- return decimal_degrees
+    decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
+    if ref == 'S' or ref == 'W':
+        decimal_degrees = -decimal_degrees
+    return decimal_degrees
 
-def _convert_to_degrees(dms): 
+
+def _convert_to_degrees(dms):
     """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
     degrees = dms[0]
     minutes = dms[1] / 60.0
     seconds = dms[2] / 3600.0
 
-    return degrees+minutes+seconds
+    return degrees + minutes + seconds
+
 
 def get_exif_data(image):
     exif_data = {}
@@ -35,6 +38,7 @@ def get_exif_data(image):
 
     return exif_data
 
+
 def _get_if_exist(data, key):
     if key in data:
         return data[key]
@@ -42,13 +46,12 @@ def _get_if_exist(data, key):
 
 
 def get_lat_lon_alt(exif_data):
-    
     lat = None
     lon = None
     alt = None
 
-    if "GPSInfo" in exif_data:	
-        try:	
+    if "GPSInfo" in exif_data:
+        try:
             gps_info = exif_data["GPSInfo"]
 
             gps_latitude = _get_if_exist(gps_info, 'GPSLatitude')
@@ -56,10 +59,10 @@ def get_lat_lon_alt(exif_data):
             gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
             gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
             gps_altitude = _get_if_exist(gps_info, 'GPSAltitude')
-            
+
             if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
                 lat = _convert_to_degrees(gps_latitude)
-                if gps_latitude_ref != "N":                     
+                if gps_latitude_ref != "N":
                     lat = 0 - lat
 
                 lon = _convert_to_degrees(gps_longitude)
@@ -68,7 +71,7 @@ def get_lat_lon_alt(exif_data):
             alt = float(gps_altitude)
         except:
             return None
-    return lat,lon,alt
+    return lat, lon, alt
 
 
 def get_datetime(exif_data):
@@ -79,10 +82,11 @@ def get_datetime(exif_data):
         return None
     return datetime
 
+
 def get_model(exif_data):
     model = None
     try:
-        model = exif_data["Model"] 
+        model = exif_data["Model"]
         model = model.replace('\x00', '')
         if not isinstance(model, str):
             return None
@@ -90,65 +94,72 @@ def get_model(exif_data):
         return None
     return model
 
+
 def get_iso(exif_data):
     iso = None
     try:
         iso = exif_data["ISOSpeedRatings"]
         if not isinstance(iso, int):
-            return None        
+            return None
     except:
         return None
     return iso
+
 
 def get_f_number(exif_data):
     f_number = None
     try:
         f_number = float(exif_data["FNumber"])
         if not isinstance(f_number, float):
-            return None        
+            return None
     except:
         return None
     return f_number
+
 
 def get_focal_length(exif_data):
     focal_length = None
     try:
         focal_length = float(exif_data["FocalLength"])
         if not isinstance(focal_length, float):
-            return None            
+            return None
     except:
         return None
     return focal_length
+
 
 def get_flash(exif_data):
     flash = None
     try:
         flash = exif_data["Flash"]
         if not isinstance(flash, int):
-            return None                
+            return None
     except:
         return None
     return flash
+
 
 def get_shutterspeed(exif_data):
     shutterspeed = None
     try:
         shutterspeed = float(exif_data["ShutterSpeedValue"])
         if not isinstance(shutterspeed, float):
-            return None            
+            return None
     except:
-        return None 
+        return None
     return shutterspeed
+
 
 def get_exposure_time(exif_data):
     exposure_time = None
     try:
         exposure_time = float(exif_data["ExposureTime"])
         if not isinstance(exposure_time, float):
-            return None                
+            return None
     except:
         return None
     return exposure_time
+
 
 def get_pixels_dimensions(exif_data):
     dimensions = None
@@ -157,7 +168,7 @@ def get_pixels_dimensions(exif_data):
         image_width = exif_data["ExifImageWidth"]
         image_height = exif_data["ExifImageHeight"]
         if not isinstance(image_width, int) | isinstance(image_height, int):
-            return None, None   
+            return None, None
         dimensions = str(image_width) + ' x ' + str(image_height)
         total_pixels = image_width * image_height
     except:
--- 05 outlier_detection/database.py
+++ 05 outlier_detection/database.py
@@ -10,7 +10,7 @@ from botocore.exceptions import NoCredentialsError
 def get_s3_client(AWS_access_key_id, AWS_secret_access_key):
     return boto3.client('s3', aws_access_key_id=AWS_access_key_id, aws_secret_access_key=AWS_secret_access_key)
 
-
+## incrementele database functie toevoegen 
 def clear_images_table(database, user, password, host, port):
     conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
     cur = conn.cursor()
@@ -29,25 +29,40 @@ def add_images_to_database(AWS_access_key_id, AWS_secret_access_key, AWS_bucket_
         contents = s3.list_objects(Bucket=AWS_bucket_name, Prefix=AWS_folder_name)['Contents']
     except NoCredentialsError:
         print("No AWS credentials found.")
-        return
+        return [], []
     except Exception as e:
         print("Error connecting to S3: ", e)
-        return
+        return [], []
+
+    images = []
+    filenames = []
 
     try:
         conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
         cur = conn.cursor()
     except Exception as e:
         print("Error connecting to the database: ", e)
-        return
+        return [], []
 
     for obj in contents:
         filename = obj['Key']
-        if not filename.endswith("/"):  # Ensure the file is not a directory
+        if not filename.endswith("/") and not filename.startswith(
+                "._"):  # Ensure the file is not a directory or a hidden/system file
             try:
                 s3.download_file(AWS_bucket_name, filename, '/tmp/' + filename.split('/')[-1])
                 with open('/tmp/' + filename.split('/')[-1], "rb") as img_file:
                     img_data = img_file.read()
+                    nparr = np.frombuffer(img_data, np.uint8)
+                    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                    if img is None:
+                        print(f"Unable to decode image: {filename}")
+                        continue
+                    if img.shape[2] == 4:
+                        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+                    img = cv2.resize(img, (128, 128))
+
+                images.append(img)
+                filenames.append(filename.split('/')[-1])
 
                 cur.execute(
                     "INSERT INTO images (filename, folder_name, image_data, outlier_score) VALUES (%s, %s, %s, %s)",
@@ -60,6 +75,8 @@ def add_images_to_database(AWS_access_key_id, AWS_secret_access_key, AWS_bucket_
     conn.commit()
     cur.close()
     conn.close()
+
+    return np.array(images), filenames
 
 
 def update_outlier_scores(database, user, password, host, port, filenames, outlier_scores):
@@ -75,46 +92,3 @@ def update_outlier_scores(database, user, password, host, port, filenames, outli
     conn.commit()
     cur.close()
     conn.close()
-
-
-def load_images_from_db(database, user, password, host, port):
-    # Establish a connection to the database
-    conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
-
-    cur = conn.cursor()
-
-    # Execute an SQL query to retrieve the filenames and image data for all images in the database
-    cur.execute("SELECT filename, image_data FROM images")
-
-    # Initialize lists to store the loaded images and their corresponding filenames
-    images = []
-    filenames = []
-
-    # Loop through each row of the query results
-    for filename, image_data in cur.fetchall():
-        try:
-            # Use OpenCV to read the image data as a numpy array
-            nparr = np.frombuffer(image_data, np.uint8)
-            image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-            # If the image has an alpha channel, remove it
-            if image.shape[2] == 4:
-                image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
-
-            # Resize the image to a fixed size
-            image = cv2.resize(image, (128, 128))
-
-            images.append(image)
-            filenames.append(filename)
-
-        except Exception as e:
-            print(f"Error loading image: {filename}")
-            print(str(e))
-            continue
-
-    print(f"Loaded {len(images)} images from the database.")
-
-    cur.close()
-    conn.close()
-
-    return np.array(images), filenames
