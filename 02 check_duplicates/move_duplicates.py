8c749c78e0562c5a97f6f064a921fd5a5ef36824
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-06-05 14:48:32 +0200

    Added checker for double duplicates and remove locacllly
    Former-commit-id: 1aa4843cf1ab924a848858a693604058151aa0fd

--- 02 check_duplicates/move_duplicates.py
+++ 02 check_duplicates/move_duplicates.py
@@ -8,10 +8,9 @@ def move_duplicate_images(img, duplicate, aws_src_folder, aws_dst_folder, local_
     local_src = os.path.join(cwd, local_folder, img)
     local_dst = os.path.join(cwd, local_folder, duplicate)
 
-    # move images to duplicate bucket folder
+    # move images to duplicate bucket folder and remove locally
     move_image_within_s3_bucket(aws_src_folder, aws_dst_folder, img)
-    move_image_within_s3_bucket(aws_src_folder, aws_dst_folder, duplicate)
-
-    # remove locally
     os.remove(local_src)
+
+    move_image_within_s3_bucket(aws_src_folder, aws_dst_folder, duplicate)
     os.remove(local_dst)
