b736816c46c226e55455c176b9bbcb51d5a37f69
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-06-12 02:07:13 +0200

    static import and update metadata on selct of image
    Former-commit-id: 2a95df58dc746d97c3bcb49abc85d8971b95c34a

--- 06 website/static/scripts/images.js
+++ 06 website/static/scripts/images.js
@@ -1,34 +1,13 @@
-async function updateSelectedImage(selectedImage) {
-    document.getElementById('selected-image').src = selectedImage.src;
+function displayImage(image) {
+    const selectedImage = document.getElementById("selected-image");
+    const imageMetadata = document.getElementById("image-metadata");
 
-    let original_img_src = selectedImage.src
-    let url_to_remove = `${window.location.href}static/images/`
-    let img_name = original_img_src.replace(url_to_remove, '')
-
-    const imageData = await getImageData(img_name);
-    const image = imageData[0];
-
-    const imageWeatherDate = document.querySelector("#image-date");
-    const imageCoordinates = document.querySelector("#image-coordinates")
-
-    if (image === undefined) {
-        // Add error message, if image has no lat/long
-
-    } else {
-        const date = new Date(image.img_creation_date)
-        const dateFormatted = date.toISOString().slice(0, 10);
-        imageWeatherDate.innerHTML = dateFormatted
-        imageCoordinates.innerHTML = `${image.latitude} ° N ${image.longitude} ° W`
-
-        const historicalData = await getHistoricalWeatherData(image.latitude, image.longitude, dateFormatted)
-        buildWeatherGraph(historicalData);
-    }
+    selectedImage.src = image.url;
+    selectedImage.alt = "Selected Image";
+    selectedImage.classList.add("selected-image-class");
+    imageMetadata.innerHTML = `
+        <h2>Location: ${image.latitude}, ${image.longtitude}</h2>
+        <h2>Picture taken at: ${image.date}</h2>
+    `;
 }
-
-function handleScroll(event) {
-    event.preventDefault();
-    const scrollAmount = event.deltaY;
-    event.target.scrollBy({left: scrollAmount, behavior: 'smooth'});
-}
-
 
