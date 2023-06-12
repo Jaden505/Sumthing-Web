3d2abd53b0d919ad4e27f4cfa31298c1d220cb42
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-06-12 16:17:00 +0200

    added detail page for weather graph and show graph for each image
    Former-commit-id: 34967184bdae52755c75704584441016965448a1

--- 06 website/main.py
+++ 06 website/main.py
@@ -7,11 +7,22 @@ from parse_images import get_img_urls_and_metadata
 app = Flask(__name__)
 
 app.config['images_data'] = get_img_urls_and_metadata()
-print(app.config['images_data'])
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', images=app.config['images_data'])
+
+
+@app.route('/weather/<int:image_id>', methods=['GET'])
+def image_weather(image_id):
+    image = None
+    for data in app.config['images_data']:
+        if data['id'] == image_id:
+            image = data
+            break
+
+    return render_template('image_weather.html', image=image)
 
 
 @app.route('/weather', methods=['GET'])
--- 06 website/static/scripts/images.js
+++ 06 website/static/scripts/images.js
@@ -8,6 +8,10 @@ function displayImage(image) {
     imageMetadata.innerHTML = `
         <h2>Location: ${image.latitude}, ${image.longtitude}</h2>
         <h2>Picture taken at: ${image.date}</h2>
-    `;
+        <div class="details">
+            <button class="details-btn" id="go-to-weatherchart" onclick="window.location.href='/weather/${image.id}'">See weather info</button>
+            <button class="details-btn" id="delete-image" onclick="deleteImage(${image.id})">Delete Image</button>
+        </div>    
+        `;
 }
 
